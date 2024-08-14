5c8ae9f6ce47c41d22dea161f5ef7053dabb0269
David Hooton <david@hooton.org>
2024-08-14 14:36:25 +1000

    🔧 chore(workflows/semantic-release): refactor version update logic
    - Moved the extraction of the new version to occur after the successful check
    of semantic-release.
    - Simplified the update mechanism for `pyproject.toml` to accommodate both
    macOS and Linux.
    - Ensured that the git configuration is set for commits made by the GitHub
    Actions bot.
    
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- .github/workflows/semantic-release.sh
+++ .github/workflows/semantic-release.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-# Extract the new version from package.json
-NEW_VERSION=$(grep '"version":' package.json | sed 's/.*"version": "\(.*\)",/\1/')
-echo "New version extracted: $NEW_VERSION"
-
-# Update the version in pyproject.toml
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Detected macOS"
-    sed -i '' -e "s/^version = \".*\"/version = \"$NEW_VERSION\"/" ./pyproject.toml
-else
-    echo "Detected Linux"
-    sed -i'' -e "s/^version = \".*\"/version = \"$NEW_VERSION\"/" ./pyproject.toml
-fi
-
-# Update the version in package.json
-jq --arg new_version "$NEW_VERSION" '.version = $new_version' package.json > tmp.$$.json && mv tmp.$$.json package.json
-
-# Replace the placeholder in pyproject.toml
-sed -i'' -e "s/\${nextRelease.version}/$NEW_VERSION/g" pyproject.toml
-
 # Run semantic-release
 echo "Running semantic-release..."
 npx semantic-release
 
-# If semantic-release was successful, commit the changes to a new branch
+# Check if semantic-release was successful
 if [ $? -eq 0 ]; then
-    git checkout -b release-v$NEW_VERSION
+    echo "semantic-release completed successfully"
+
+    # Extract the new version from package.json
+    NEW_VERSION=$(grep '"version":' package.json | sed 's/.*"version": "\(.*\)",/\1/')
+    echo "New version: $NEW_VERSION"
+
+    # Update the version in pyproject.toml
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Detected macOS"
+        sed -i '' -e "s/^version = \".*\"/version = \"$NEW_VERSION\"/" ./pyproject.toml
+    else
+        echo "Detected Linux"
+        sed -i -e "s/^version = \".*\"/version = \"$NEW_VERSION\"/" ./pyproject.toml
+    fi
+
+    # Commit and push the changes
+    git config --local user.email "github-actions[bot]@users.noreply.github.com"
+    git config --local user.name "github-actions[bot]"
     git add pyproject.toml
     git commit -m "chore: update pyproject.toml to $NEW_VERSION [skip ci]"
-    git push origin release-v$NEW_VERSION
+    git push
 else
     echo "semantic-release failed"
+    exit 1
 fi
